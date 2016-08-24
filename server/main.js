21c235892e9106dac8ad92edc174bcf0b5a18012
Stephen Mayeux <stephenmayeux@gmail.com>
2016-08-25 01:28:08 +0900

    seed the db

--- server/main.js
+++ server/main.js
@@ -1,5 +1,18 @@
 import { Meteor } from 'meteor/meteor';
+import _ from 'lodash';
+import { Profiles } from '../imports/collections/profiles';
+import { image, helpers } from 'faker';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  // seed db
+  const numRecords = Profiles.find({}).count();
+  if (!numRecords) {
+    _.times(25, () => {
+      const { name, email, phone } = helpers.createCard();
+      Profiles.insert({
+        name, email, phone,
+        avatar: image.avatar()
+      });
+    });
+  }  
 });
